d042de4f90822ea59e00e541f1282efff5e08eaf
Lane Pemberton <lanebpemberton@gmail.com>
2021-07-01 01:02:33 -0400

    updated models with datatypes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    }
   },
   {
     sequelize,
